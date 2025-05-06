2658e31ab2f66c44380600f162d68846a440a2be
Bschaler <90104387+Bschaler@users.noreply.github.com>
2025-05-05 21:11:54 -0400

    Prepare for deployment: update routes and build frontend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,10 +2,38 @@ const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
 
-router.get('/', (req, res) => {
-  res.json({ message: 'WEBSITE INCOMING!!!' });
-});
-
 router.use('/api', apiRouter);
 
-module.exports = router;        
+
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
+    );
+  });
+
+
+  router.use(express.static(path.resolve("../frontend/dist")));
+
+
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
+    );
+  });
+}
+if (process.env.NODE_ENV !== 'production') {
+  router.get("/api/csrf/restore", (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+      'XSRF-Token': csrfToken
+    });
+  });
+}
+
+module.exports = router;      
